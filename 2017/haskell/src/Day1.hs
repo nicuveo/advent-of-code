dd5b4eb1d4f2f4174a78fd7ca1107b56be8b80cd
Nicuvëo <crucuny@gmail.com>
2017-12-02 08:33:08 +0000

    Added 2017 - haskell - day1.

--- 2017/haskell/src/Day1.hs
+++ 2017/haskell/src/Day1.hs
@@ -7,10 +7,12 @@ module Day1 (day1) where
 -- import
 
 import           Common
+import           Data.Char
 
 
 
 -- solution
 
 day1 :: Solution
-day1 input = undefined
+day1 l1@(x:xs) = show $ sum [ digitToInt a | (a,b) <- zip l1 l2, a == b ]
+  where l2 = xs ++ [x]
