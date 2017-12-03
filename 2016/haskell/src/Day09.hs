be3d73d66f0de4797a94ea78108e0676eba59845
Nicuvëo <crucuny@gmail.com>
2017-12-03 00:50:16 +0000

    Added 2016 - haskell - 9.

--- 2016/haskell/src/Day09.hs
+++ 2016/haskell/src/Day09.hs
@@ -6,6 +6,10 @@ module Day09 (day09_1, day09_2) where
 
 -- import
 
+import           Control.Monad
+import           Data.Either.Utils
+import           Text.Parsec
+
 import           Common
 
 
@@ -13,13 +17,39 @@ import           Common
 -- solution
 
 day09_1 :: Solution
-day09_1 = undefined
+day09_1 = show . parseData1 . join . lines
 
 
 day09_2 :: Solution
-day09_2 = undefined
+day09_2 = show . parseData2 . join . lines
 
 
 
 -- helpers
 
+parseData1 :: String -> Int
+parseData1 = sum . fromRight . parse (many text) ""
+  where text = try repeated <|> expanded
+        repeated = do
+          (t, p) <- repeatedPattern
+          return $ t * length p
+
+parseData2 :: String -> Int
+parseData2 = sum . fromRight . parse (many text) ""
+  where text = try repeated <|> expanded
+        repeated = do
+          (t, p) <- repeatedPattern
+          return $ t * parseData2 p
+
+expanded :: Parsec String u Int
+expanded = length <$> many1 upper
+
+repeatedPattern :: Parsec String u (Int, String)
+repeatedPattern = do
+  void $ char '('
+  n <- read <$> many1 digit
+  void $ char 'x'
+  t <- read <$> many1 digit
+  void $ char ')'
+  p <- count n anyChar
+  return (t, p)
