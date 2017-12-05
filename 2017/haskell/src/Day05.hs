931e213e3735b9e2a6a7a1bc0a3b81c1889312f6
Nicuvëo <crucuny@gmail.com>
2017-12-05 05:27:22 +0000

    Added 2017 - haskell - 5.

--- 2017/haskell/src/Day05.hs
+++ 2017/haskell/src/Day05.hs
@@ -8,18 +8,39 @@ module Day05 (day05_1, day05_2) where
 
 import           Common
 
+import           Data.Array
+
 
 
 -- solution
 
 day05_1 :: Solution
-day05_1 = undefined
-
+day05_1 = show . jump 0 0 . toArray . map readInt . lines
+  where jump jumps inst steps
+          | inst `within` bounds steps =
+              let d = steps ! inst
+              in  jump (jumps + 1) (inst + d) $ steps // [(inst, d + 1)]
+          | otherwise = jumps
 
 day05_2 :: Solution
-day05_2 = undefined
+day05_2 = show . jump 0 0 . toArray . map readInt . lines
+  where jump jumps inst steps
+          | inst `within` bounds steps =
+              let d = steps ! inst
+              in  if d > 2
+                  then jump (jumps + 1) (inst + d) $ steps // [(inst, d - 1)]
+                  else jump (jumps + 1) (inst + d) $ steps // [(inst, d + 1)]
+          | otherwise = jumps
 
 
 
 -- helpers
 
+readInt :: String -> Int
+readInt = read
+
+toArray :: [a] -> Array Int a
+toArray l = listArray (0, length l -1) l
+
+within :: Ord a => a -> (a, a) -> Bool
+x `within` (l, u) = l <= x && x <= u
