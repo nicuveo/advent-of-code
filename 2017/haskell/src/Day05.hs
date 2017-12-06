3097434319add917e5bdef8f76992979fb701601
Nicuvëo <crucuny@gmail.com>
2017-12-06 10:51:26 +0000

    Used Unboxed ST arrays instead of normal arrays, for performance.

--- 2017/haskell/src/Day05.hs
+++ 2017/haskell/src/Day05.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
+
+
+
 -- module
 
 module Day05 (day05_1, day05_2) where
@@ -6,41 +10,48 @@ module Day05 (day05_1, day05_2) where
 
 -- import
 
-import           Common
+import           Control.Monad.ST
+import           Data.Array.ST
 
-import           Data.Array
+import           Common
 
 
 
 -- solution
 
 day05_1 :: Solution
-day05_1 = show . jump 0 0 . toArray . map readInt . lines
-  where jump jumps inst steps
-          | inst `within` bounds steps =
-              let d = steps ! inst
-              in  jump (jumps + 1) (inst + d) $ steps // [(inst, d + 1)]
-          | otherwise = jumps
+day05_1 input = show $ runST $ jump 0 0 =<< toArray (map readInt $ lines input)
+  where jump !jumps !inst steps = do
+          bs  <- getBounds steps
+          if not $ inst `within` bs
+            then return (jumps :: Int)
+            else do
+              d <- readArray steps inst
+              writeArray steps inst $ d + 1
+              jump (jumps + 1) (inst + d) steps
 
 day05_2 :: Solution
-day05_2 = show . jump 0 0 . toArray . map readInt . lines
-  where jump jumps inst steps
-          | inst `within` bounds steps =
-              let d = steps ! inst
-              in  if d > 2
-                  then jump (jumps + 1) (inst + d) $ steps // [(inst, d - 1)]
-                  else jump (jumps + 1) (inst + d) $ steps // [(inst, d + 1)]
-          | otherwise = jumps
+day05_2 input = show $ runST $ jump 0 0 =<< toArray (map readInt $ lines input)
+  where jump !jumps !inst steps = do
+          bs  <- getBounds steps
+          if not $ inst `within` bs
+            then return jumps
+            else do
+              d <- readArray steps inst
+              writeArray steps inst $ if d > 2 then d - 1 else d + 1
+              jump (jumps + 1) (inst + d) steps
 
 
 
 -- helpers
 
+type IntArray s = STUArray s Int Int
+
 readInt :: String -> Int
 readInt = read
 
-toArray :: [a] -> Array Int a
-toArray l = listArray (0, length l -1) l
+toArray :: [Int] -> ST s (IntArray s)
+toArray l = newListArray (0, length l -1) l
 
 within :: Ord a => a -> (a, a) -> Bool
 x `within` (l, u) = l <= x && x <= u
