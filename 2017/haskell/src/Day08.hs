ed37df82f2bd16d885579fb2a1303b0c4c236d0e
Nicuvëo <crucuny@gmail.com>
2017-12-09 03:42:53 +0000

    Added 2017 - Haskell - 08.

--- 2017/haskell/src/Day08.hs
+++ 2017/haskell/src/Day08.hs
@@ -6,6 +6,10 @@ module Day08 (day08_1, day08_2) where
 
 -- import
 
+import           Data.List
+import           Data.Map.Strict hiding (foldl', map)
+import           Text.Parsec
+
 import           Common
 
 
@@ -13,13 +17,65 @@ import           Common
 -- solution
 
 day08_1 :: Solution
-day08_1 = undefined
-
+day08_1 = show . regMax . foldl' (flip execute) empty . map compile . lines
 
 day08_2 :: Solution
-day08_2 = undefined
+day08_2 = show . maximum . map regMax . scanl' (flip execute) empty . map compile . lines
 
 
 
 -- helpers
 
+type Registers = Map String Int
+
+data Instruction = Inst { _iReg   :: String
+                        , _iDelta :: Int
+                        , _iTest  :: String
+                        , _iCond  :: Int -> Bool
+                        }
+
+compile :: String -> Instruction
+compile = either (error . show) id . parse line ""
+  where name = many1 lower
+        val  = read <$> many1 (oneOf "-0123456789")
+        mul  = try inc <|> dec
+        op   = try eq <|> try ne <|>
+               try le <|> try ge <|>
+               try lt <|> gt
+        inc  = string "inc" >> return 1
+        dec  = string "dec" >> return (-1)
+        eq   = string "=="  >> return (==)
+        ne   = string "!="  >> return (/=)
+        le   = string "<="  >> return (<=)
+        ge   = string ">="  >> return (>=)
+        lt   = string "<"   >> return (<)
+        gt   = string ">"   >> return (>)
+        line = do
+          reg <- name
+          spaces
+          coeff <- mul
+          spaces
+          delta <- val
+          spaces
+          string "if"
+          spaces
+          comp <- name
+          spaces
+          operator <- op
+          spaces
+          value <- val
+          return $ Inst reg (coeff * delta) comp (`operator` value)
+
+regMax :: Registers -> Int
+regMax = foldl' max 0 . elems
+
+regGet :: String -> Registers -> Int
+regGet = findWithDefault 0
+
+regSet :: String -> Int -> Registers -> Registers
+regSet = insertWith (+)
+
+execute :: Instruction -> Registers -> Registers
+execute (Inst reg delta test cond) regs = if cond $ regGet test regs
+                                          then regSet reg delta regs
+                                          else regs
