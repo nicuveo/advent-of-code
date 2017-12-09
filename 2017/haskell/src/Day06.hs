934df3d05fcb0750c6552d737db716a767489e28
Nicuvëo <crucuny@gmail.com>
2017-12-09 02:06:52 +0000

    Refactored 2017 - haskell - 06.

--- 2017/haskell/src/Day06.hs
+++ 2017/haskell/src/Day06.hs
@@ -24,28 +24,10 @@ import           Common
 -- solution
 
 day06_1 :: Solution
-day06_1 input = show $ runST $ walk1 0 [] =<< readBanks input
-  where walk1 !step hist banks = do
-          this <- freeze banks
-          if this `elem` hist
-            then return step
-            else do
-              let (size, maxVal, start) = extract this
-              writeArray banks start 0
-              redistribute banks maxVal size (start + 1)
-              walk1 (step + 1) (this : hist) banks
+day06_1 input = show $ fst $ runST $ walk 0 [] =<< readBanks input
 
 day06_2 :: Solution
-day06_2 input = show $ runST $ walk2 [] =<< readBanks input
-  where walk2 hist banks = do
-          this <- freeze banks
-          case elemIndex this hist of
-            Just n  -> return $ n + 1
-            Nothing -> do
-              let (size, maxVal, start) = extract this
-              writeArray banks start 0
-              redistribute banks maxVal size (start + 1)
-              walk2 (this : hist) banks
+day06_2 input = show $ snd $ runST $ walk 0 [] =<< readBanks input
 
 
 
@@ -59,10 +41,19 @@ toArray l = newListArray (0, length l -1) l
 readBanks :: String -> ST s (IntMArray s)
 readBanks = toArray . map read . words
 
-extract :: Array Int Int -> (Int, Int, Int)
-extract banks = (length blocks, maxVal, elemIndexJust maxVal blocks)
-  where blocks = elems banks
-        maxVal = maximum blocks
+walk :: Int -> [Array Int Int] -> IntMArray s -> ST s (Int, Int)
+walk !step hist banks = do
+  this <- freeze banks
+  case elemIndex this hist of
+    Just n  -> return (step, n + 1)
+    Nothing -> do
+      let blocks = elems this
+          size   = length blocks
+          maxVal = maximum blocks
+          start  = elemIndexJust maxVal blocks
+      writeArray banks start 0
+      redistribute banks maxVal size (start + 1)
+      walk (step + 1) (this : hist) banks
 
 redistribute :: IntMArray s -> Int -> Int -> Int -> ST s ()
 redistribute banks left size x = when (left > 0) $ do
