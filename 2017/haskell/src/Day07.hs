19cc25d8a40e11c518e7e6b02e86ca951337b2d3
Nicuvëo <crucuny@gmail.com>
2017-12-09 02:12:37 +0000

    Added 2017 - Haskell - 07.

--- 2017/haskell/src/Day07.hs
+++ 2017/haskell/src/Day07.hs
@@ -6,6 +6,14 @@ module Day07 (day07_1, day07_2) where
 
 -- import
 
+import           Data.Either.Utils
+import           Data.Function
+import           Data.List
+import           Data.Map.Strict   hiding (null, (\\))
+import           Data.Maybe
+import           Data.Tree
+import           Text.Parsec
+
 import           Common
 
 
@@ -13,13 +21,55 @@ import           Common
 -- solution
 
 day07_1 :: Solution
-day07_1 = undefined
+day07_1 = plateName . rootLabel . mapToTree . parseTower
 
 
 day07_2 :: Solution
-day07_2 = undefined
+day07_2 = show . lAnomaly . walk . mapToTree . parseTower
+  where walk (Node plate forest)
+          | null forest = let w = plateWeight plate in Level w w 0
+          | otherwise   = let w = plateWeight plate
+                              f = sortOn length $ groupBy ((==) `on` lTotalWeight) $ walk <$> forest
+                          in case f of
+                               [balanced] -> Level w (w + sum (lTotalWeight <$> balanced)) $ sum (lAnomaly <$> balanced)
+                               [[Level p t 0], rest@(Level _ r 0:_)] -> Level w (w + sum (lTotalWeight <$> rest) + r) $ r - t + p
+                               _ -> error $ "erroneous group: " ++ show f
 
 
 
 -- helpers
 
+data Plate = Plate { plateName   :: String
+                   , plateWeight :: Int
+                   }
+
+data Level = Level { _lPlateWeight :: Int
+                   , lTotalWeight  :: Int
+                   , lAnomaly      :: Int
+                   }
+
+instance Show Level where
+  show (Level p t a) = show (p, t, a)
+
+type TowerTree = Tree Plate
+type TowerMap  = Map String (Int, [String])
+
+parseTower :: String -> TowerMap
+parseTower input = fromList $ fromRight . parse line "" <$> lines input
+  where name   = many1 lower
+        weigth = between (char '(') (char ')') $ many1 digit
+        arrow  = spaces >> string "->" >> spaces
+        line = do
+          n <- name
+          spaces
+          w <- read <$> weigth
+          p <- fmap (fromMaybe []) $ optionMaybe $ do
+            arrow
+            name `sepBy1` string ", "
+          return (n, (w, p))
+
+mapToTree :: TowerMap -> TowerTree
+mapToTree towerMap = unfoldTree walk root
+  where root = head $ keys towerMap \\ (elems towerMap >>= snd)
+        walk name = let (weight, forest) = towerMap ! name
+                    in (Plate name weight, forest)
