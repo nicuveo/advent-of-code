b690d45ecdc11c82550cb39e3a2ae1bd3f5abd65
Nicuvëo <crucuny@gmail.com>
2017-12-13 11:23:17 +0000

    Refactored 2017 - Haskell - 13 to not use lists anymore.

--- 2017/haskell/src/Day13.hs
+++ 2017/haskell/src/Day13.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
+
+
+
 -- module
 
 module Day13 (day13_1, day13_2) where
@@ -8,38 +12,34 @@ module Day13 (day13_1, day13_2) where
 
 import           Common
 
-import           Data.List
-import           Safe
 import           Text.Parsec
-import           Text.Printf
 
 
 
 -- solution
 
 day13_1 :: Solution
-day13_1 input  = show $ sum [severity l | l <- layers, at (scanner l) (depth l) == 0]
-  where layers = parseLayer <$> lines input
+day13_1 = show . sum . map severity . filter (isCaught 0) . map parseLayer . lines
 
 day13_2 :: Solution
-day13_2 input  = show $ head [delay | (delay, plan) <- zip [0..] plans, 0 `notElem` plan]
-  where layers = parseLayer <$> lines input
-        plans  = transpose [drop (depth l) $ scanner l | l <- layers]
+day13_2 = show . delay 0 . map parseLayer . lines
+  where delay !d l
+          | any (isCaught d) l = delay (d+1) l
+          | otherwise          = d
 
 
 
 -- helpers
 
-data Layer = Layer { depth   :: Int
-                   , range   :: Int
-                   , scanner :: [Int]
-                   }
-
-instance Show Layer where
-  show l = printf "%2d:%s" (depth l) $ show $ take 30 $ scanner l
+data Layer = Layer { depth :: Int
+                   , range :: Int
+                   } deriving (Show)
 
 severity :: Layer -> Int
 severity l = depth l * range l
+
+isCaught :: Int -> Layer -> Bool
+isCaught delay l = (depth l + delay) `mod` (2 * range l - 2) == 0
 
 parseLayer :: String -> Layer
 parseLayer = parseWith line
@@ -47,4 +47,4 @@ parseLayer = parseWith line
           d <- intParser
           string ": "
           r <- intParser
-          return $ Layer d r $ cycle $ [0..r-1] ++ [r-2,r-3..1]
+          return $ Layer d r
