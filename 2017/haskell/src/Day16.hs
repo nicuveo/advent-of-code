8bc184313617b6b98c655dc21c2d414ff34bd33e
Nicuvëo <crucuny@gmail.com>
2017-12-16 18:33:40 +0000

    Moved the number of dancers to the type system.

--- 2017/haskell/src/Day16.hs
+++ 2017/haskell/src/Day16.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures    #-}
+
+
+
 -- module
 
 module Day16 ( day16_MV1_1, day16_MV1_2
@@ -19,6 +25,7 @@ import           Data.List                   as L hiding (elemIndex, splitAt,
 import           Data.Maybe
 import           Data.Vector.Unboxed         as V hiding (foldM)
 import           Data.Vector.Unboxed.Mutable as V hiding (splitAt)
+import           GHC.TypeLits
 import           Prelude                     hiding (splitAt, (++))
 import           Test.QuickCheck
 import           Text.Parsec
@@ -32,22 +39,23 @@ import           Common
 -- solutions MV2: same, but with instruction packing
 
 day16_MV1_1 :: Solution
-day16_MV1_1 = danceMV 16 1 . parseInstructions
+day16_MV1_1 = danceMV 1 . parseInstructions
 
 day16_MV1_2 :: Solution
-day16_MV1_2 = danceMV 16 1000000000 . parseInstructions
+day16_MV1_2 = danceMV 1000000000 . parseInstructions
 
 day16_MV2_1 :: Solution
-day16_MV2_1 = danceMV 16 1 . pack 16 . parseInstructions
+day16_MV2_1 = danceMV 1 . pack . parseInstructions
 
 day16_MV2_2 :: Solution
-day16_MV2_2 = danceMV 16 1000000000 . pack 16 . parseInstructions
+day16_MV2_2 = danceMV 1000000000 . pack . parseInstructions
 
 
-danceMV :: Int -> Int -> [Instruction] -> String
-danceMV n times insts =
+danceMV :: KnownNat n => Int -> [Instruction n] -> String
+danceMV times insts =
   printf "MV, after %3d dance(s) of %5d instructions: %s " steps (L.length insts) $ toList res
-  where (steps, res) = runST $ do
+  where n = instSize $ L.head insts
+        (steps, res) = runST $ do
           origin <- thaw $ fromList $ mkDancers n
           backup <- freeze origin
           _dance times 0 backup origin
@@ -60,27 +68,29 @@ danceMV n times insts =
           then return (i :: Int, current)
           else do
             ds <- unsafeThaw current
-            _dance (nt-1) (i+1) b =<< foldM (executeMV n) ds insts
+            _dance (nt-1) (i+1) b =<< foldM executeMV ds insts
 
-executeMV :: Int -> MVector s Name -> Instruction -> ST s (MVector s Name)
-executeMV n dancers (Spin x) = doSpin n x <$> unsafeFreeze dancers >>= unsafeThaw
-executeMV _ dancers (Exchange a b) = unsafeSwap dancers a b >> return dancers
-executeMV _ dancers (Partner x y) = do
+executeMV :: KnownNat n => MVector s Name -> Instruction n -> ST s (MVector s Name)
+executeMV dancers i@(Spin x) = doSpin (instSize i) x <$> unsafeFreeze dancers >>= unsafeThaw
+executeMV dancers (Exchange a b) = unsafeSwap dancers a b >> return dancers
+executeMV dancers (Partner x y) = do
   names <- unsafeFreeze dancers
   let a = fromJust $ elemIndex x names
       b = fromJust $ elemIndex y names
   newDancers <- unsafeThaw names
   unsafeSwap newDancers a b
   return newDancers
-executeMV n dancers (UpdateI l) = do
+executeMV dancers i@(UpdateI l) = do
+  let n = instSize i
   result <- new n
   old    <- unsafeFreeze dancers
   V.forM_ (indexed l) $ \(newi, oldi) -> unsafeWrite result newi $ unsafeIndex old oldi
   return result
-executeMV n dancers (UpdateN l) = do
-  M.forM_ [0..n-1] $ \i -> do
-    c <- unsafeRead dancers i
-    unsafeWrite dancers i $ unsafeIndex l $ ord c - ord 'a'
+executeMV dancers i@(UpdateN l) = do
+  let n = instSize i
+  M.forM_ [0..n-1] $ \x -> do
+    c <- unsafeRead dancers x
+    unsafeWrite dancers x $ unsafeIndex l $ ord c - ord 'a'
   return dancers
 
 
@@ -89,22 +99,23 @@ executeMV n dancers (UpdateN l) = do
 -- solutions IV2: same, but with instruction packing
 
 day16_IV1_1 :: Solution
-day16_IV1_1 = danceIV 16 1 . parseInstructions
+day16_IV1_1 = danceIV 1 . parseInstructions
 
 day16_IV1_2 :: Solution
-day16_IV1_2 = danceIV 16 1000000000 . parseInstructions
+day16_IV1_2 = danceIV 1000000000 . parseInstructions
 
 day16_IV2_1 :: Solution
-day16_IV2_1 = danceIV 16 1 . pack 16 . parseInstructions
+day16_IV2_1 = danceIV 1 . pack . parseInstructions
 
 day16_IV2_2 :: Solution
-day16_IV2_2 = danceIV 16 1000000000 . pack 16 . parseInstructions
+day16_IV2_2 = danceIV 1000000000 . pack . parseInstructions
 
 
-danceIV :: Int -> Int -> [Instruction] -> String
-danceIV n times insts =
+danceIV :: KnownNat n => Int -> [Instruction n] -> String
+danceIV times insts =
   printf "IV, after %3d dance(s) of %5d instructions: %s " steps (L.length insts) $ toList res
-  where start = fromList $ mkDancers n
+  where n = instSize $ L.head insts
+        start = fromList $ mkDancers n
         (steps, res) = _dance times 0 start
         _dance t i dancers =
           let nt = if t < times && start == dancers
@@ -112,14 +123,14 @@ danceIV n times insts =
                    else t
           in  if nt == 0
               then (i :: Int, dancers)
-              else _dance (nt-1) (i+1) $ L.foldl' (executeIV n) dancers insts
+              else _dance (nt-1) (i+1) $ L.foldl' executeIV dancers insts
 
-executeIV :: Int -> Vector Name -> Instruction -> Vector Name
-executeIV n dancers (Spin x)       = doSpin n x dancers
-executeIV _ dancers (Exchange a b) = doExchange a b dancers
-executeIV _ dancers (Partner x y)  = doPartners x y dancers
-executeIV _ dancers (UpdateI l)    = V.map (unsafeIndex dancers) l
-executeIV _ dancers (UpdateN l)    = flip V.map dancers $ \c -> unsafeIndex l $ ord c - ord 'a'
+executeIV :: KnownNat n => Vector Name -> Instruction n -> Vector Name
+executeIV dancers i@(Spin x)     = doSpin (instSize i) x dancers
+executeIV dancers (Exchange a b) = doExchange a b dancers
+executeIV dancers (Partner x y)  = doPartners x y dancers
+executeIV dancers (UpdateI l)    = V.map (unsafeIndex dancers) l
+executeIV dancers (UpdateN l)    = flip V.map dancers $ \c -> unsafeIndex l $ ord c - ord 'a'
 
 
 
@@ -127,7 +138,7 @@ executeIV _ dancers (UpdateN l)    = flip V.map dancers $ \c -> unsafeIndex l $ 
 
 type Name = Char
 
-data Instruction
+data Instruction (n :: Nat)
   -- unpacked
   = Spin     Int
   | Exchange Int  Int
@@ -138,8 +149,13 @@ data Instruction
   deriving Show
 
 
-noop :: Int -> Instruction
-noop = UpdateI . enumFromN 0
+instSize :: KnownNat n => Instruction n -> Int
+instSize = fromInteger . natVal
+
+noop :: KnownNat n => Instruction n
+noop = i
+  where i = UpdateI $ enumFromN 0 n
+        n = instSize i
 
 doSpin :: Unbox a => Int -> Int -> Vector a -> Vector a
 doSpin n x v = end ++ beginning
@@ -159,8 +175,8 @@ mkDancers :: Int -> [Name]
 mkDancers n = chr <$> [start .. start + n - 1]
   where start = ord 'a'
 
-pack :: Int -> [Instruction] -> [Instruction]
-pack n insts = [indexInst, namesInst]
+pack :: KnownNat n => [Instruction n] -> [Instruction n]
+pack insts = [indexInst, namesInst]
   where dancers = mkDancers n
         (onNames, onIndex) = L.partition isPartner insts
         isPartner (Partner _ _) = True
@@ -170,10 +186,11 @@ pack n insts = [indexInst, namesInst]
         combineI _           _              = error "day16: unexpected instructions when packing"
         combineP d           (Partner x y)  = doPartners x y d
         combineP _           _              = error "day16: unexpected instructions when packing"
-        indexInst = L.foldl' combineI (noop n) onIndex
+        indexInst = L.foldl' combineI noop onIndex
         namesInst = UpdateN $ L.foldl' combineP (fromList dancers) onNames
+        n = instSize indexInst
 
-parseInstructions :: String -> [Instruction]
+parseInstructions :: String -> [Instruction 16]
 parseInstructions = parseWith $ inst `sepBy` char ','
   where inst = tryAll [spin, exch, part]
         spin = do
@@ -196,7 +213,7 @@ parseInstructions = parseWith $ inst `sepBy` char ','
 
 -- tests
 
-instance Arbitrary Instruction where
+instance Arbitrary (Instruction 16) where
   arbitrary = oneof [arbitraryS, arbitraryE, arbitraryP]
     where arbitraryI = elements [0..15]
           arbitraryN = elements $ mkDancers 16
@@ -205,5 +222,6 @@ instance Arbitrary Instruction where
           arbitraryE = Exchange <$> arbitraryI <*> arbitraryI
 
 day16_tests :: IO ()
-day16_tests = quickCheck $ \l -> solve l == solve (pack 16 l)
-  where solve = L.foldl' (executeIV 16) $ fromList $ mkDancers 16
+day16_tests = quickCheck $ \l -> solve l == solve (pack l)
+  where solve :: [Instruction 16] -> Vector Name
+        solve = L.foldl' executeIV $ fromList $ mkDancers 16
