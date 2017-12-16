dd7b8f0b320f467ba18a37461627c252a51834a7
Nicuvëo <crucuny@gmail.com>
2017-12-16 00:32:20 +0000

    Added naive version of 2017 - Haskell - 15.

--- 2017/haskell/src/Day15.hs
+++ 2017/haskell/src/Day15.hs
@@ -6,6 +6,9 @@ module Day15 (day15_1, day15_2) where
 
 -- import
 
+import           Data.Bits
+import           Text.Parsec
+
 import           Common
 
 
@@ -13,13 +16,42 @@ import           Common
 -- solution
 
 day15_1 :: Solution
-day15_1 = undefined
+day15_1 input = show $ countTrue $ zipWith (==) valsA valsB
+  where [startValueA, startValueB] = parseStartValue <$> lines input
+        valsA = generate maxIter1 1 genAFactor startValueA
+        valsB = generate maxIter1 1 genBFactor startValueB
 
 
 day15_2 :: Solution
-day15_2 = undefined
+day15_2 input = show $ countTrue $ zipWith (==) valsA valsB
+  where [startValueA, startValueB] = parseStartValue <$> lines input
+        valsA = generate maxIter2 4 genAFactor startValueA
+        valsB = generate maxIter2 8 genBFactor startValueB
 
 
 
 -- helpers
 
+genAFactor, genBFactor, divider, maxIter1, maxIter2 :: Int
+genAFactor =      16807
+genBFactor =      48271
+divider    = 2147483647
+maxIter1   =   40000000
+maxIter2   =    5000000
+
+simplify :: Int -> Int
+simplify x = x .&. 0xFFFF
+
+generate :: Int -> Int -> Int -> Int -> [Int]
+generate iter cmod factor start =
+  map simplify $ take iter $ filter (isMod cmod) $ drop 1 $ iterate step start
+  where step val  = mod (factor * val) divider
+        isMod m x = mod x m == 0
+
+parseStartValue :: String -> Int
+parseStartValue = parseWith line
+  where line = do
+          string "Generator "
+          anyChar
+          string " starts with "
+          intParser
