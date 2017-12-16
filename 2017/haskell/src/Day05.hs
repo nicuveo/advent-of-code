308e106824f8cebe638b532f524fd1de800650d7
Nicuvëo <crucuny@gmail.com>
2017-12-16 00:31:29 +0000

    Refactored 2017 - haskell - 05.

--- 2017/haskell/src/Day05.hs
+++ 2017/haskell/src/Day05.hs
@@ -20,26 +20,12 @@ import           Common
 -- solution
 
 day05_1 :: Solution
-day05_1 input = show $ runST $ jump 0 0 =<< toArray (map readInt $ lines input)
-  where jump !jumps !inst steps = do
-          bs  <- getBounds steps
-          if not $ inst `within` bs
-            then return (jumps :: Int)
-            else do
-              d <- readArray steps inst
-              writeArray steps inst $ d + 1
-              jump (jumps + 1) (inst + d) steps
+day05_1 input = show $ runST $ jump update 0 0 =<< toArray (map readInt $ lines input)
+  where update d = d + 1
 
 day05_2 :: Solution
-day05_2 input = show $ runST $ jump 0 0 =<< toArray (map readInt $ lines input)
-  where jump !jumps !inst steps = do
-          bs  <- getBounds steps
-          if not $ inst `within` bs
-            then return jumps
-            else do
-              d <- readArray steps inst
-              writeArray steps inst $ if d > 2 then d - 1 else d + 1
-              jump (jumps + 1) (inst + d) steps
+day05_2 input = show $ runST $ jump update 0 0 =<< toArray (map readInt $ lines input)
+  where update d = if d > 2 then d - 1 else d + 1
 
 
 
@@ -52,3 +38,13 @@ toArray l = newListArray (0, length l -1) l
 
 within :: Ord a => a -> (a, a) -> Bool
 x `within` (l, u) = l <= x && x <= u
+
+jump :: (Int -> Int) -> Int -> Int -> IntArray s -> ST s Int
+jump update !jumps !inst steps = do
+  bs  <- getBounds steps
+  if not $ inst `within` bs
+    then return jumps
+    else do
+      d <- readArray steps inst
+      writeArray steps inst $ update d
+      jump update (jumps + 1) (inst + d) steps
