248a859e6d1c3fc05079369f36d63de3b223de76
Nicuvëo <crucuny@gmail.com>
2018-06-16 23:27:14 +0100

    Added AoC 2016's Day18 in Haskell.

--- 2016/haskell/src/Day18.hs
+++ 2016/haskell/src/Day18.hs
@@ -1,10 +1,13 @@
 -- module
 
-module Day18 (day18_1, day18_2) where
+module Day18 (day18_1, day18_2, prettyPrint) where
 
 
 
 -- import
+
+import           Data.Maybe
+import qualified Data.Vector.Unboxed as V
 
 import           Common
 
@@ -13,13 +16,34 @@ import           Common
 -- solution
 
 day18_1 :: Solution
-day18_1 = undefined
+day18_1 = show . sum . map countSafe . take 40 . iterate nextRow . parseInput
 
 
 day18_2 :: Solution
-day18_2 = undefined
+day18_2 = show . sum . map countSafe . take 400000 . iterate nextRow . parseInput
 
 
 
 -- helpers
 
+type Row = V.Vector Bool
+
+countSafe :: Row -> Int
+countSafe = V.length . V.filter id
+
+nextRow :: Row -> Row
+nextRow r = V.imap nextTile r
+  where nextTile i _ = let left  = fromMaybe True $ r V.!? (i-1)
+                           right = fromMaybe True $ r V.!? (i+1)
+                       in  left == right
+
+parseInput :: String -> Row
+parseInput = V.fromList . map isSafe
+  where isSafe '.' = True
+        isSafe '^' = False
+        isSafe _   = error "parseInput: malformed input!"
+
+prettyPrint :: Row -> String
+prettyPrint = map toChar . V.toList
+  where toChar True  = '.'
+        toChar False = '^'
