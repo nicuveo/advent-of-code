3307c878caeaae4a38867bf27fc7ea9f3cbe644c
Nicuvëo <crucuny@gmail.com>
2018-06-16 22:01:13 +0100

    AoC 2016's Day 14 in Haskell.

--- 2016/haskell/src/Day14.hs
+++ 2016/haskell/src/Day14.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
+
+
+
 -- module
 
 module Day14 (day14_1, day14_2) where
@@ -6,6 +10,11 @@ module Day14 (day14_1, day14_2) where
 
 -- import
 
+import           Crypto.Hash.MD5        (hash)
+import           Data.ByteString.Base16 (encode)
+import           Data.ByteString.Char8  (pack, unpack)
+import           Data.List
+
 import           Common
 
 
@@ -13,13 +22,40 @@ import           Common
 -- solution
 
 day14_1 :: Solution
-day14_1 = undefined
+day14_1 salt = show $ findKeys 1 salt !! 63
 
 
 day14_2 :: Solution
-day14_2 = undefined
+day14_2 salt = show $ findKeys 2017 salt !! 63
 
 
 
--- helpers
+-- md5
 
+type Salt = String
+
+md5 :: Int -> Salt -> Int -> String
+md5 count salt index = unpack $ foldl' step first [1..count]
+  where step accum _ = encode $ hash accum
+        first        = pack $ salt ++ show index
+
+
+
+-- keys
+
+triplet :: String -> Maybe Char
+triplet (a:b:c:s)
+  | a == b && b == c = Just a
+  | otherwise        = triplet $ b:c:s
+triplet _ = Nothing
+
+hasQuintuplet :: Char -> String -> Bool
+hasQuintuplet c s = any ([c,c,c,c,c] `isPrefixOf`) $ tails s
+
+findKeys :: Int -> Salt -> [Int]
+findKeys count salt = findKeys_ 0 $ map (md5 count salt) [0..]
+  where findKeys_ _      [] = error "findKeys: empty infinite list!?"
+        findKeys_ !index (key:rest)
+          | Just t <- triplet key,
+            any (hasQuintuplet t) $ take 1000 rest = index : findKeys_ (index+1) rest
+          | otherwise                              =         findKeys_ (index+1) rest
