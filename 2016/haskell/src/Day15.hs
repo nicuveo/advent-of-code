ff18e04a0a5951b2ebe52bc4fa1ecf64fcbb19bd
Nicuvëo <crucuny@gmail.com>
2018-06-16 22:13:11 +0100

    Added AoC 2016's Day15 in Haskell.

--- 2016/haskell/src/Day15.hs
+++ 2016/haskell/src/Day15.hs
@@ -6,6 +6,8 @@ module Day15 (day15_1, day15_2) where
 
 -- import
 
+import           Text.Parsec
+
 import           Common
 
 
@@ -13,13 +15,35 @@ import           Common
 -- solution
 
 day15_1 :: Solution
-day15_1 = undefined
+day15_1 input = show $ head $ filter check [0..]
+  where discs = parseInput input
+        check index = and [ mod (start + number + index) positions == 0
+                          | (number, positions, start) <- discs
+                          ]
 
 
 day15_2 :: Solution
-day15_2 = undefined
+day15_2 input = show $ head $ filter check [0..]
+  where discs = parseInput input ++ [(7, 11, 0)]
+        check index = and [ mod (start + number + index) positions == 0
+                          | (number, positions, start) <- discs
+                          ]
 
 
 
--- helpers
+-- parsing
 
+type Disc = (Int, Int, Int)
+
+parseInput :: String -> [Disc]
+parseInput = parseWith discs
+  where discs = disc `sepBy1` many1 newline
+        disc  = do
+          symbol "Disc #"
+          number <- intParser
+          symbol "has"
+          positions <- intParser
+          symbol "positions; at time=0, it is at position"
+          start <- intParser
+          symbol "."
+          return (number, positions, start)
