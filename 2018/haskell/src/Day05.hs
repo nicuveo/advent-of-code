aba7924521439a94ece61d6da12cd26b20f603d0
Nicuvëo <crucuny@gmail.com>
2018-12-10 22:27:15 +0000

    Added Day 05 of AoC 2018 in Haskell.

--- 2018/haskell/src/Day05.hs
+++ 2018/haskell/src/Day05.hs
@@ -6,6 +6,9 @@ module Day05 (day05_1, day05_2) where
 
 -- import
 
+import           Data.Char
+import           Data.Function
+
 import           Common
 
 
@@ -13,13 +16,30 @@ import           Common
 -- solution
 
 day05_1 :: Solution
-day05_1 = undefined
+day05_1 = show . length . foldr react []
 
 
 day05_2 :: Solution
-day05_2 = undefined
+day05_2 input = show $ minimum
+                [ length $ foldr react [] $ removeUnit x firstPass
+                | x <- ['a'..'z']
+                ]
+  where firstPass = foldr react [] input
 
 
 
 -- helpers
 
+react :: Char -> String -> String
+react unit [] = [unit]
+react unit (otherUnit : reacted) =
+  if unit `reactsWith` otherUnit
+  then                    reacted
+  else unit : otherUnit : reacted
+
+removeUnit :: Char -> String -> String
+removeUnit c = filter $ isNot c
+  where isNot y x = y /= toLower x
+
+reactsWith :: Char -> Char -> Bool
+reactsWith a b = on (==) toLower a b && a /= b
