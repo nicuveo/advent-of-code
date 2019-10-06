1d98d96bb6f247d931085a171c224074ebae10b3
Nicuvëo <crucuny@gmail.com>
2019-10-06 16:16:03 +0100

    Moved all info to AnimationState record.

--- 2019/haskell/lib/AOC/Debug/Animate.hs
+++ 2019/haskell/lib/AOC/Debug/Animate.hs
@@ -61,6 +61,39 @@ defaultDelay = 1000
 
 
 
+-- animation state
+
+data AnimState a = AS { originalDelay :: Milliseconds
+                      , currentDelay  :: Milliseconds
+                      , originalState :: a
+                      , stateHistory  :: [a]
+                      , currentIndex  :: Int
+                      , isPaused      :: Bool
+                      , canContinue   :: Bool
+                      , frameCount    :: Int
+                      }
+
+mkAnimState :: Milliseconds -> a -> AnimState a
+mkAnimState d a = AS d d a [a] 0 False True 0
+
+getCurrent :: AnimState a -> a
+getCurrent as = stateHistory as !! currentIndex as
+
+insertState :: AnimState a -> a -> AnimState a
+insertState as a = assert (currentIndex as == 0) $
+                   as { frameCount   = frameCount as + 1
+                      , stateHistory = a : take 20 (stateHistory as)
+                      }
+
+stepForwards :: UpdateFun a -> AnimState a -> IO (AnimState a)
+stepForwards f s
+  | currentIndex s > 0 = return $ s { frameCount   = frameCount   s + 1
+                                    , currentIndex = currentIndex s - 1
+                                    }
+  | otherwise = fmap (maybe s (insertState s)) $ f $ getCurrent s
+
+
+
 -- keyboard handling
 
 data Action = Pause
@@ -77,16 +110,17 @@ data Action = Pause
 peekActions :: IO [Action]
 peekActions = fmap catMaybes $ whileM (hReady stdin) $ do
   c <- getChar
-  case c of
-    ' ' -> return $ Just Pause
-    '+' -> return $ Just Faster
-    '-' -> return $ Just Slower
-    '=' -> return $ Just ResetSpeed
-    'p' -> return $ Just PrevFrame
-    'n' -> return $ Just NextFrame
-    'q' -> return $ Just Quit
-    '?' -> return $ Just Help
-    _   -> print [c] >> return Nothing
+  return  $ case c of
+              ' ' -> Just Pause
+              '+' -> Just Faster
+              '-' -> Just Slower
+              '=' -> Just ResetSpeed
+              'r' -> Just Restart
+              'p' -> Just PrevFrame
+              'n' -> Just NextFrame
+              '?' -> Just Help
+              'q' -> Just Quit
+              _   -> Nothing
 
 
 
@@ -119,29 +153,32 @@ printHelp = do
   void getChar
   clearScreen >> resetCursor
 
-processActions :: Milliseconds -> Milliseconds -> IO (Milliseconds, Bool, Bool)
-processActions original current = peekActions >>= foldM exec (current, False, True)
-  where exec (d, p, c) Faster     = return (max  100 $ round $ fromIntegral d * 0.80, p, c)
-        exec (d, p, c) Slower     = return (min 2000 $ round $ fromIntegral d * 1.25, p, c)
-        exec (d, p, c) ResetSpeed = return (original, p, c)
-        exec (d, p, c) Quit       = return (d, p, False)
-        exec s         Help       = printHelp >> return s
-        exec s         _          = return s
+processActions :: AnimState a -> IO (AnimState a)
+processActions animState = peekActions >>= foldM exec animState
+  where exec as Faster     = return $ as { currentDelay = faster $ currentDelay as }
+        exec as Slower     = return $ as { currentDelay = slower $ currentDelay as }
+        exec as ResetSpeed = return $ as { currentDelay = originalDelay as }
+        exec as Pause      = return $ as { isPaused     = not $ isPaused as }
+        exec as Quit       = return $ as { canContinue  = False }
+        exec as Help       = printHelp >> return as
+        exec as _          = return as
+        faster d = max  100 $ round $ fromIntegral d * 0.80
+        slower d = min 2000 $ round $ fromIntegral d * 1.25
 
 animate :: Milliseconds -> RenderFun a -> UpdateFun a -> a -> IO ()
-animate originalDelay render step initialState = do
+animate delay render step initialState = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdout False
   clearScreen
   channel <- newEmptyMVar
-  run originalDelay channel (0 :: Int) initialState
-  where run delay channel !n !object = do
+  run channel (0 :: Int) $ mkAnimState delay initialState
+  where run channel !n !state = do
           clearScreen >> resetCursor
-          (delayMs, _, continue) <- processActions originalDelay delay
-          printf "Delay: %dms\nFrame: #%d\n" delayMs n
-          putStr $ render object
-          when continue $ do
-            forkIO $ async channel $ step object
-            sleep delayMs
+          newState <- processActions state
+          printf "Delay: %dms\nFrame: #%d\n" (currentDelay newState) n
+          putStr $ render $ getCurrent newState
+          when (canContinue newState) $ do
+            forkIO $ async channel $ step $ getCurrent newState
+            sleep $ currentDelay newState
             whenM (isEmptyMVar channel) $ putStrLn "(waiting...)"
-            whenJustM (takeMVar channel) $ run delayMs channel $ n+1
+            whenJustM (takeMVar channel) $ run channel (n+1) . insertState newState
