6979976e7c03f8a32a3fefa945fca2121f0b61e6
Nicuvëo <crucuny@gmail.com>
2019-10-07 23:47:59 +0100

    ...added nyan cat?

--- 2019/haskell/src/AnimationTest.hs
+++ 2019/haskell/src/AnimationTest.hs
@@ -1,46 +1,25 @@
-{-# LANGUAGE TupleSections #-}
+import           Codec.Picture.Gif
+import           Codec.Picture.Types
+import           Data.ByteString     as B (readFile)
 
-import           Data.Bool
-import           Data.Ratio
-import           System.Random
-
-import           AOC
 import           AOC.Debug
-import           AOC.Map.Flat
 
 
-type GameOfLife = FlatMap Bool
-
-
-tlColor = interpolate (2%3) white red
-trColor = interpolate (2%3) white yellow
-blColor = interpolate (2%3) white green
-brColor = interpolate (2%3) white blue
-
-w = 50
-h = 50
-
-
-mkGame :: StdGen -> GameOfLife
-mkGame = makeFlatMapFromList w h . take (w*h) . randoms
-
-display :: GameOfLife -> String
-display = displayWith $ \p ->  bool "  " $ fgColor (colorOf p) "##"
-  where colorOf (Point y x) =
-          let c1 = interpolate (x % (w-1)) tlColor trColor
-              c2 = interpolate (x % (w-1)) blColor brColor
-          in interpolate (y % (h-1)) c1 c2
-
-step :: GameOfLife -> GameOfLife
-step g = flip pmap g $ \p b -> status b $ countTrue $ eightMapNeighboursOf g p
-  where status True  x = x `elem` [2,3]
-        status False x = x == 3
-
+readGif :: FilePath -> IO [String]
+readGif f = map toString . either error id . decodeGifImages <$> B.readFile f
+  where toString (ImageRGB8 i@(Image w h _)) =
+          unlines [ concat [ flip bgColor "  " $ toColor $ pixelAt i x y
+                           | x <- [0..w-1]
+                           ]
+                  | y <- [0..h-1]
+                  ]
+        toString _ = error "cannot happen"
+        toColor (PixelRGB8 r g b) = RGB r g b
 
 main :: IO ()
 main = do
-  n <- getStdGen
-  let s = return ([], mkGame n)
-      r = display
-      u = return . Just . ([],) . step
-  animate defaultDelay resetCursor r u s
+  frames <- readGif "nyan-cat.gif"
+  let n   = length frames
+      u x = return $ Just ([], mod (x+1) n)
+      r x = frames !! x
+  animate 90 resetCursor r u $ return ([], 0)
