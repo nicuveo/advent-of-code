594634c35b2621c9994c6f7ca561213bc98cb0b2
Nicuvëo <crucuny@gmail.com>
2019-10-08 00:40:39 +0100

    AnimationTest is now the game of life.

--- 2019/haskell/src/AnimationTest.hs
+++ 2019/haskell/src/AnimationTest.hs
@@ -1,67 +1,39 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns     #-}
+{-# LANGUAGE TupleSections #-}
 
-import           Control.Concurrent
-import           Control.Monad.State
+import           Data.Bool
 import           System.Random
 
+import           AOC
 import           AOC.Debug.Animate
+import           AOC.Map.Flat
 
 
-type TestMonad m = StateT StdGen m
-type IColor = (Int, Int, Int)
+type GameOfLife = FlatMap Bool
 
 
-getRandom :: Monad m => (StdGen -> (a, StdGen)) -> TestMonad m a
-getRandom f = do
-  (a,g) <- gets f
-  put g
-  return a
+mkGame :: StdGen -> GameOfLife
+mkGame = makeFlatMapFromList w h . take (w*h) . randoms
+  where w = 50
+        h = 50
 
-randomColor :: Monad m => TestMonad m IColor
-randomColor = do
-  r <- getRandom $ randomR (0, 255)
-  g <- getRandom $ randomR (0, 255)
-  b <- getRandom $ randomR (0, 255)
-  return (r,g,b)
+display :: GameOfLife -> String
+display g =
+  unlines [ concat [ bool "  " "##" $ g ! Point y x
+                   | x <- mapXs g
+                   ]
+          | y <- mapYs g
+          ]
 
-interpolate :: IColor -> IColor -> Int -> IColor
-interpolate (r1, g1, b1) (r2, g2, b2) w =
-  ( div (r1 * w + r2 * (19 - w)) 19
-  , div (g1 * w + g2 * (19 - w)) 19
-  , div (b1 * w + b2 * (19 - w)) 19
-  )
+step :: GameOfLife -> GameOfLife
+step g = flip pmap g $ \p b -> status b $ countTrue $ eightMapNeighboursOf g p
+  where status True  x = x `elem` [2,3]
+        status False x = x == 3
 
-toRGB :: IColor -> Color
-toRGB ( toEnum -> r
-      , toEnum -> g
-      , toEnum -> b
-      ) = RGB r g b
-
-randomPicture :: Monad m => TestMonad m (Logs, String)
-randomPicture = do
-  rs <- get
-  tl <- randomColor
-  bl <- randomColor
-  tr <- randomColor
-  br <- randomColor
-  let lineColors = [(interpolate tl bl y, interpolate tr br y) | y <- [0..19]]
-      result =  unlines [ concat [ flip bgColor "  " $ toRGB $ interpolate lc rc x
-                                 | x <- [0..19]
-                                 ]
-                        | (lc, rc) <- lineColors
-                        ]
-  return (["StdGen: '" ++ show rs ++ "'"], result)
-
-getPicture :: Monad m => StdGen -> TestMonad m (Logs, String)
-getPicture s = put s >> randomPicture
-
-nextRandomPicture :: String -> TestMonad IO (Maybe (Logs, String))
-nextRandomPicture _ = do
-  liftIO $ threadDelay 200000
-  Just <$> randomPicture
 
 main :: IO ()
 main = do
-  s <- getStdGen
-  flip evalStateT s $ animate defaultDelay id nextRandomPicture $ getPicture s
+  n <- getStdGen
+  let s = return ([], mkGame n)
+      r = display
+      u = return . Just . ([],) . step
+  animate defaultDelay r u s
