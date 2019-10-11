e5fe876b29f3e90f50e3671e87ee61f0b32fc346
Nicuvëo <crucuny@gmail.com>
2019-10-12 00:28:18 +0100

    Added list strictness primitives.

--- 2019/haskell/lib/AOC/List.hs
+++ 2019/haskell/lib/AOC/List.hs
@@ -13,7 +13,7 @@ import           AOC.Misc
 
 
 
--- list helpers
+-- counting elements
 
 countTrue :: [Bool] -> Int
 countTrue = countIf id
@@ -24,6 +24,10 @@ countIf = length ... filter
 count :: Eq a => a -> [a] -> Int
 count a = length . filter (a ==)
 
+
+
+-- on equivalents for by functions
+
 minimumOn :: Ord b => (a -> b) -> [a] -> a
 minimumOn = minimumBy . on compare
 
@@ -32,3 +36,13 @@ maximumOn = maximumBy . on compare
 
 groupOn :: Ord b => (a -> b) -> [a] -> [[a]]
 groupOn = groupBy . on (==)
+
+
+
+-- strictness operations
+
+forceSpine :: [a] -> [a]
+forceSpine = foldr (const id) () <. seq
+
+forceElements :: [a] -> [a]
+forceElements = foldr seq () <. seq
