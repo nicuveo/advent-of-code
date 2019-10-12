33529670721b10d4ac1d14b645d7934d6e77c26c
Nicuvëo <crucuny@gmail.com>
2019-10-12 12:34:26 +0100

    Added enumerate.

--- 2019/haskell/lib/AOC/Misc.hs
+++ 2019/haskell/lib/AOC/Misc.hs
@@ -4,6 +4,13 @@ module AOC.Misc where
 
 
 
+-- enumeration
+
+enumerate :: (Bounded a, Enum a) => [a]
+enumerate = [minBound .. maxBound]
+
+
+
 -- the blackbird
 
 (...) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
