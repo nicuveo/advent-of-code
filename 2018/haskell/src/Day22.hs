7b97d5e0c84d8963a9e0d1df684197059c5d9c38
Nicuvëo <crucuny@gmail.com>
2019-11-29 14:52:50 +0000

    Ugly solution to Day22.

--- 2018/haskell/src/Day22.hs
+++ 2018/haskell/src/Day22.hs
@@ -6,20 +6,91 @@ module Day22 (day22_1, day22_2) where
 
 -- import
 
+import           Control.Monad
+import           Data.Function
+import qualified Data.Map             as M
+import           Data.MemoCombinators
+
 import           Common
+import           PathFinding
 
 
 
 -- solution
 
 day22_1 :: Solution
-day22_1 = undefined
+day22_1 s = show $ sum [fromEnum $ regionType i (y,x) | y <- [0 .. ty], x <- [0 .. tx]]
+  where i@(_, (ty, tx)) = parseInput s
 
 
 day22_2 :: Solution
-day22_2 = undefined
+day22_2 = show . shortestPath . parseInput
+
+-- shortestPath :: Input -> Int
+shortestPath i@(_, target@(ty,tx)) = findPathH neighbours heuristic (0,0,Torch) (ty,tx,Torch)
+  where neighbours (y,x,t) = (7, (y,x,other t $ regionType i (y,x))) : do
+          (ny,nx) <- [(y-1,x), (y,x+1), (y+1,x), (y,x-1)]
+          let nk = regionType i (ny,nx)
+          guard $ ny >= 0 && nx >= 0
+          guard $ t `allowedIn` nk
+          return (1, (ny,nx,t))
+        heuristic (y,x,t) = abs (y - ty) + abs(x - tx) + (if t == Torch then 0 else 7)
+
+
 
 
 
 -- helpers
 
+type Point = (Int, Int)
+type Seed  = Int
+type Input = (Seed, Point)
+
+parseInput :: String -> Input
+parseInput = parseWith $ liftM2 (,) depth target
+  where depth  = symbol "depth:" >> intParser
+        target = do
+          symbol "target:"
+          x <- intParser
+          symbol ","
+          y <- intParser
+          return (y,x)
+
+z = (`mod` 20183)
+
+geoLevel :: Input -> Point -> Int
+geoLevel = memo2 (pair integral point) point gl
+  where point = pair integral integral
+        gl i@(_, target) p@(y,x)
+          | target == p = 0
+          | y == 0      = x * 16807
+          | x == 0      = y * 48271
+          | otherwise   = z $ erosionLevel i (y-1,x) * erosionLevel i (y,x-1)
+
+
+erosionLevel :: Input -> Point -> Int
+erosionLevel i@(seed, _) p = z $ geoLevel i p + seed
+
+regionType :: Input -> Point -> Type
+regionType = (toEnum . (`mod` 3)) ... erosionLevel
+
+
+data Type = Rocky | Wet | Narrow
+  deriving (Eq, Ord, Enum, Bounded)
+
+instance Show Type where
+  show Rocky  = "."
+  show Wet    = "="
+  show Narrow = "|"
+
+data Tool = Neither | Torch | Gear
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+allowedIn :: Tool -> Type -> Bool
+allowedIn Neither Rocky  = False
+allowedIn Torch   Wet    = False
+allowedIn Gear    Narrow = False
+allowedIn _       _      = True
+
+other :: Tool -> Type -> Tool
+other t k = head [nt | nt <- [minBound .. maxBound], nt /= t, nt `allowedIn` k]
---
+++ 2018/haskell/src/PathFinding.hs
@@ -0,0 +1,155 @@
+{-# LANGUAGE BangPatterns #-}
+
+module PathFinding ( PFState
+                   , mkPFState
+                   , pfStart
+                   , pfEnd
+                   , pfCurrent
+                   , pfCost
+                   , pfParent
+                   , pfFinished
+                   , pfFound
+                   , pfMap
+
+                   , pfStep
+                   , pfPath
+
+                   , findPath
+                   , maybeFindPath
+                   , findPathH
+                   , maybeFindPathH
+                   ) where
+
+
+
+-- imports
+
+import           Data.Function   (on)
+import           Data.List       as L
+import           Data.Map.Lazy   as M
+import           Data.Maybe
+import           Data.PQueue.Min as Q
+
+
+
+-- cell type
+
+newtype Cell a = Cell (Int, Int, a) deriving Eq
+
+instance Eq a => Ord (Cell a) where
+  compare = compare `on` \(Cell (e,c,_)) -> (e,-c)
+
+instance Show a => Show (Cell a) where
+  show (Cell (e, c, a)) = concat [ "{e: "
+                                 , show e
+                                 , ", "
+                                 , show c
+                                 , ", "
+                                 , show a
+                                 , "}"
+                                 ]
+
+cellValue :: Cell a -> a
+cellValue (Cell (_, _, a)) = a
+
+
+
+-- path finder state
+
+data PFState a = PFS { pfN :: a -> [(Int, a)]     -- outward edges with cost
+                     , pfH :: a -> Int            -- heuristic function
+                     , pfS :: a                   -- start
+                     , pfE :: a                   -- end
+                     , pfQ :: Q.MinQueue (Cell a) -- internal queue of cells
+                     , pfP :: M.Map a (a, Int)    -- cell' parent & cost
+                     }
+
+
+mkPFState :: Ord a => (a -> [(Int, a)]) -> (a -> Int) -> a -> a -> PFState a
+mkPFState n h s e = PFS n h s e q p
+  where q = Q.singleton $ Cell (h s, 0, s)
+        p = M.singleton s (s, 0)
+
+pfStart :: PFState a -> a
+pfStart = pfS
+
+pfEnd :: PFState a -> a
+pfEnd = pfE
+
+pfCurrent :: PFState a -> a
+pfCurrent = cellValue . Q.findMin . pfQ
+
+pfCost :: Ord a => PFState a -> a -> Maybe Int
+pfCost s x = snd <$> pfP s !? x
+
+pfParent :: Ord a => PFState a -> a -> Maybe a
+pfParent s x = fst <$> pfP s !? x
+
+pfMap :: PFState a -> M.Map a (a, Int)
+pfMap = pfP
+
+
+pfFinished :: PFState a -> Bool
+pfFinished s = Q.null (pfQ s)
+
+pfFound :: Ord a => PFState a -> Bool
+pfFound s = pfFinished s && pfE s `M.member` pfP s
+
+
+
+-- path finding functions
+
+pfStep :: Ord a => PFState a -> PFState a
+pfStep !s
+  | pfFinished s = s
+  | (Cell (_, c, a), q') <- Q.deleteFindMin $ pfQ s =
+      if a == pfE s
+      then s { pfQ = Q.empty }
+      else skipToNext $ L.foldl' (insertElt a c) (s { pfQ = q' }) $ pfN s a
+  where skipToNext st = st { pfQ = Q.dropWhile (shouldIgnore st) $ pfQ st }
+        shouldIgnore st (Cell (_, c, a)) = c > snd (pfP st ! a)
+
+pfPath :: Ord a => PFState a -> [(a, Int)]
+pfPath s
+  | pfFound s = fill (pfEnd s) []
+  | otherwise = []
+  where fill cell path = if cell == pfS s
+                         then path'
+                         else fill p path'
+          where (p,c) = pfP s ! cell
+                path' = (cell, c) : path
+
+
+
+-- internal helpers
+
+insertElt :: Ord a => a -> Int -> PFState a -> (Int, a) -> PFState a
+insertElt parent cost st (distance, neighb)
+  | maybe True (\(_,c) -> neighbCost < c) $ pfP st !? neighb =
+      st { pfQ = Q.insert neighbCell $ pfQ st
+         , pfP = M.insert neighb (parent, neighbCost) $ pfP st
+         }
+  | otherwise = st
+  where neighbCost = cost + distance
+        neighbCell = Cell ( neighbCost + pfH st neighb
+                          , neighbCost
+                          , neighb)
+
+
+
+-- putting it together
+
+findPath :: Ord a => (a -> [(Int, a)]) -> a -> a -> [(a, Int)]
+findPath = flip findPathH $ const 0
+
+maybeFindPath :: Ord a => (a -> [(Int, a)]) -> a -> a -> Maybe [(a, Int)]
+maybeFindPath = flip maybeFindPathH $ const 0
+
+findPathH :: Ord a => (a -> [(Int, a)]) -> (a -> Int) -> a -> a -> [(a, Int)]
+findPathH n h s e = fromMaybe (error "findPath: no path found") $ maybeFindPathH n h s e
+
+maybeFindPathH :: Ord a => (a -> [(Int, a)]) -> (a -> Int) -> a -> a -> Maybe [(a, Int)]
+maybeFindPathH n h s e = finalize $ until pfFinished pfStep $ mkPFState n h s e
+  where finalize w
+          | pfFound w = Just $ pfPath w
+          | otherwise = Nothing
