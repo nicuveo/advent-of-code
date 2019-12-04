d2fb64402aa8abcb506179da5343613f25e70753
Nicuvëo <crucuny@gmail.com>
2019-12-04 00:26:56 +0000

    Added Day03.

--- 2019/haskell/src/Day03.hs
+++ 2019/haskell/src/Day03.hs
@@ -1,8 +1,7 @@
 -- import
 
-import           Data.Function (on)
-import           Data.List
-import           Data.Maybe
+import qualified Data.Map    as M
+import qualified Data.Set    as S
 import           Text.Parsec
 
 import           AOC
@@ -11,24 +10,62 @@ import           AOC
 
 -- input
 
-type Input = String
+type Input = [[Vector]]
 
 parseInput :: String -> Input
 parseInput = map parseLine . lines
   where parseLine = parseWith line
-        line = undefined
+        line = vector `sepBy` char ','
+        vector = do
+          d <- direction
+          s <- intLiteral
+          return $ s .* directionVector d
+        direction = choice [ char 'U' >> return N
+                           , char 'R' >> return E
+                           , char 'D' >> return S
+                           , char 'L' >> return W
+                           ]
 
 
 
--- solution
+-- common
 
-part1 :: Input -> String
-part1 = undefined
-  where f = undefined
+manhattanDistance :: Point -> Int
+manhattanDistance (Point y x) = abs y + abs x
 
-part2 :: Input -> String
-part2 = undefined
-  where f = undefined
+tracePath :: [Vector] -> [Point]
+tracePath = foldl line [Point 0 0]
+  where line path v =
+          let u = signum v
+              d = manhattanDistance v
+              p = last path
+          in  path ++ [p + n .* u | n <- [1 .. d]]
+
+
+
+-- part 1
+
+part1 :: Input -> Int
+part1 input = S.findMin $ S.map manhattanDistance $ S.intersection s1 s2
+  where [w1, w2] = input
+        s1 = pathSet $ tracePath w1
+        s2 = pathSet $ tracePath w2
+
+pathSet :: [Point] -> S.Set Point
+pathSet = S.fromList . tail
+
+
+
+-- part 2
+
+part2 :: Input -> Int
+part2 input = minimum $ M.elems $ M.intersectionWith (+) m1 m2
+  where [w1, w2] = input
+        m1 = pathMap $ tracePath w1
+        m2 = pathMap $ tracePath w2
+
+pathMap :: [Point] -> M.Map Point Int
+pathMap path = M.fromListWith const $ tail $ zip path [0..]
 
 
 
