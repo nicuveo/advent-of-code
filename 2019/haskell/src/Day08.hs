15a07e7de15939aacc8df3904f4aaf9492c87260
Nicuvëo <crucuny@gmail.com>
2019-12-09 22:02:34 +0000

    Day 8

--- 2019/haskell/src/Day08.hs
+++ 2019/haskell/src/Day08.hs
@@ -1,9 +1,9 @@
 -- import
 
-import           Data.Function (on)
+import           Data.Char
 import           Data.List
+import           Data.List.Split
 import           Data.Maybe
-import           Text.Parsec
 
 import           AOC
 
@@ -11,24 +11,32 @@ import           AOC
 
 -- input
 
-type Input = String
+type Input = [Layer]
+type Layer = [Int]
+
+width, height :: Int
+width  = 25
+height = 6
 
 parseInput :: String -> Input
-parseInput = map parseLine . lines
-  where parseLine = parseWith line
-        line = undefined
+parseInput = chunksOf (width * height) . map digitToInt
 
 
 
 -- solution
 
-part1 :: Input -> String
-part1 = undefined
-  where f = undefined
+part1 :: Input -> Int
+part1 layers = count 1 l * count 2 l
+  where l = minimumOn (count 0) layers
 
 part2 :: Input -> String
-part2 = undefined
-  where f = undefined
+part2 layers = unlines $ chunksOf width image
+  where findPixel = fromMaybe 2 . find (/= 2)
+        image     = map (toChar . findPixel) $ transpose layers
+        toChar 0 = ' '
+        toChar 1 = '#'
+        toChar 2 = '.'
+        toChar _ = error "wut"
 
 
 
@@ -38,4 +46,4 @@ main :: IO ()
 main = aocMain 8 $ \rawInput -> do
   let input = parseInput rawInput
   print $ part1 input
-  print $ part2 input
+  putStrLn $ part2 input
