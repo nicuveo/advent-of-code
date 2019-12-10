cbd2366bf6618f0af3e001d1664d131fec279e15
Nicuvëo <crucuny@gmail.com>
2019-12-10 00:34:20 +0000

    Added better meta tests to Day01.

--- 2019/haskell/src/Day01.hs
+++ 2019/haskell/src/Day01.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 -- import
 
+import           Data.String.QQ
 import qualified Data.Vector.Unboxed as V
 
 import           AOC
@@ -22,23 +25,28 @@ parseInput = map read . lines
 
 program :: String
 
-program = unlines [ "index = 6"
-                  , "print @8"
-                  , "@8 = 42"
-                  , "print @8"
-                  , "print $index"
-                  , "$index = 42"
-                  , "print $index"
-                  , "print &index"
-                  , "print index"
-                  , "read mass"
-                  , "total = 0"
-                  , "while (mass >= 0) {"
-                  , "  total = total + mass / 3 - 2"
-                  , "  read mass"
-                  , "}"
-                  , "print total"
-                  ]
+program = [s|
+/*
+  index = 6
+  print @8
+  @8 = 42
+  print @8
+  print $index
+  $index = 42
+  print $index
+  print &index
+  print index
+*/
+
+  read remaining
+  total = 0
+  while (remaining > 0) {
+    read mass
+    total = total + mass / 3 - 2
+    remaining = remaining - 1
+  }
+  print total
+|]
 
 
 -- solution
@@ -56,22 +64,23 @@ fuelRequired mass = max 0 $ div mass 3 - 2
 
 -- main
 
+runWithInput :: [Int] -> [[Int]] -> [Int]
+runWithInput prog inputs = snd $ run (V.fromList prog) $ prefixLength =<< inputs
+  where prefixLength l = length l : l
+
 main :: IO ()
 main = aocMain 1 $ \rawInput -> do
   let input = parseInput rawInput
   print $ part1 input
   print $ part2 input
+  putStrLn "======"
+  putStrLn ""
 
-  -- let ic = either error id $ transpile "day01" program
-  -- print ic
-  -- let (p, o) = run (V.fromList ic) (input ++ [-1])
-  -- print p
-  -- print o
-
-  let interpreter   = makeInterpreter ++ replicate 1000 0
-      compiled      = either error id $ transpile "day01" program
-      combinedInput = length compiled : compiled ++ input ++ [-1]
+  let interpreter   = makeInterpreter
+      day01         = either error id $ transpile "day01" program
+      fakeInput     = [23,5,7]
   print $ length interpreter
-  print compiled
-  print $ snd $ run (V.fromList compiled) $ input ++ [-1]
-  print $ snd $ run (V.fromList interpreter) combinedInput
+  print $ runWithInput day01       [fakeInput]
+  print $ runWithInput interpreter [day01, fakeInput]
+  print $ runWithInput interpreter [interpreter, day01, fakeInput]
+  print $ runWithInput interpreter [interpreter, interpreter, day01, fakeInput]
