b9a418c60bc952974760963edcc8f2a48d53902e
Nicuvëo <crucuny@gmail.com>
2019-12-12 21:34:38 +0000

    Day 10.

--- 2019/haskell/src/Day10.hs
+++ 2019/haskell/src/Day10.hs
@@ -2,8 +2,6 @@
 
 import           Data.Function (on)
 import           Data.List
-import           Data.Maybe
-import           Text.Parsec
 
 import           AOC
 
@@ -11,24 +9,50 @@ import           AOC
 
 -- input
 
-type Input = String
+type Asteroids = [Point]
 
-parseInput :: String -> Input
-parseInput = map parseLine . lines
-  where parseLine = parseWith line
-        line = undefined
+parseInput :: String -> Asteroids
+parseInput input = [ Point y x
+                   | (y, row) <- zip [0..] $ lines input
+                   , (x, c)   <- zip [0..] row
+                   , c == '#'
+                   ]
 
 
 
 -- solution
 
-part1 :: Input -> String
-part1 = undefined
-  where f = undefined
+reduce :: Point -> Vector
+reduce (Point 0 0) = error "overlapping asteroids"
+reduce (Point 0 x) = Point 0 $ signum x
+reduce (Point y 0) = Point (signum y) 0
+reduce (Point y x) = Point (div y d) (div x d)
+  where d = gcd y x
 
-part2 :: Input -> String
-part2 = undefined
-  where f = undefined
+
+part1 :: Asteroids -> (Int, Point)
+part1 asteroids = maximum $ zip (map canView asteroids) asteroids
+  where canView a = length $ nub [ getVector a b
+                                 | b <- asteroids
+                                 , b /= a
+                                 ]
+        getVector p q = reduce $ q - p
+
+
+part2 :: Asteroids -> Point -> (Double, Int, Point)
+part2 asteroids station = (!! 199)      $
+                          concat        $
+                          transpose     $
+                          groupOn angle $
+                          sort          $
+                          map addInfo   $
+                          filter (/= station) asteroids
+  where addInfo :: Point -> (Double, Int, Point)
+        addInfo p =
+          let r = p - station
+              Point y x = reduce r
+          in (-(atan2 `on` fromIntegral) x y, sqNorm r, p)
+        angle (a, _, _) = a
 
 
 
@@ -37,5 +61,6 @@ part2 = undefined
 main :: IO ()
 main = aocMain 10 $ \rawInput -> do
   let input = parseInput rawInput
-  print $ part1 input
-  print $ part2 input
+      (part1Result, station) = part1 input
+  print part1Result
+  print $ part2 input station
