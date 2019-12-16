0e1c9646f67902bbefd9f16cb60762090a7df7e9
Nicuvëo <crucuny@gmail.com>
2019-12-16 23:01:27 +0000

    Finished day 16!

--- 2019/haskell/src/Day16.hs
+++ 2019/haskell/src/Day16.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE BangPatterns     #-}
 {-# LANGUAGE ParallelListComp #-}
 
 
 -- import
 
-import           Data.Char (digitToInt)
+import           Control.Parallel.Strategies
+import           Data.Char                   (digitToInt)
+import           Data.List
 
 import           AOC
 
@@ -20,14 +23,28 @@ parseInput = map digitToInt
 
 -- solution
 
-digitPattern :: Int -> [Int]
-digitPattern = (patterns !!)
-  where patterns    = map mkPattern [1..]
-        mkPattern n = tail $ cycle $ [0,1,0,-1] >>= replicate n
+truncatedDigitPattern :: Int -> [Int]
+truncatedDigitPattern = (patterns !!)
+  where patterns    = map (tail . mkPattern) [1..]
+        mkPattern n = cycle $ [1,0,-1,0] >>= replicate n
 
 step :: Signal -> Signal
-step s = [f i | i <- [0..] | _ <- s]
-  where f i = flip mod 10 $ abs $ sum $ zipWith (*) s $ digitPattern i
+step s = withStrategy (parList rseq)
+  [ flip mod 10 $ abs $ a + sum (zipWith (*) b $ truncatedDigitPattern i)
+  | i <- [0..]
+  | a:b <- tails s
+  ]
+
+showSignal :: Int -> Signal -> String
+showSignal = concatMap show ... take
+
+part1 :: Signal -> String
+part1 = showSignal 8 . (!! 100) . iterate step
+
+part2 :: Signal -> String
+part2 = showSignal 8 . run 100
+  where run  0 = id
+        run !n = run (n-1) . scanr1 (\ !a !b -> mod (a+b) 10)
 
 
 
@@ -35,6 +52,9 @@ step s = [f i | i <- [0..] | _ <- s]
 
 main :: IO ()
 main = aocMain 16 $ \rawInput -> do
-  let input = parseInput rawInput
-      z = iterate step input
-  putStrLn $ take 8 $ concatMap show $ z !! 100
+  let input  = parseInput rawInput
+      offset = read $ showSignal 7 input
+      signal = drop offset $ concat $ replicate 10000 input
+
+  putStrLn $ "Part1: " ++ part1 input
+  putStrLn $ "Part2: " ++ part2 signal
