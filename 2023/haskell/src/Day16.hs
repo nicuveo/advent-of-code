2f2dc697b2e99f5e8379cd35287bedfb17714586
Antoine Leblanc <crucuny@gmail.com>
2023-12-19 21:31:27 +0000

    2023: haskell: day 16

--- 2023/haskell/src/Day16.hs
+++ 2023/haskell/src/Day16.hs
@@ -4,39 +4,110 @@ module Day16 where
 -- import
 
 import AOC
+import AOC.Grid.Flat
 import "this" Prelude
 
-import Text.Parsec
-import Text.Parsec.Char
+import Control.Monad.Loops (iterateUntilM)
+import Data.HashSet        qualified as S
 
 
 -- input
 
-type Input = String
+type Input = Grid Char
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = from2DList . lines
 
 
 -- solution
 
+type OrientedPoint = (Point, Direction)
+type SolveState    = HashSet OrientedPoint
+type SolveMonad m  = (MonadReader Input m, MonadState SolveState m)
+
+inspect :: SolveMonad m => Point -> m Char
+inspect p = asks (! p)
+
+step :: SolveMonad m => OrientedPoint -> m [OrientedPoint]
+step (p, d) = do
+  directions <-
+    inspect p <&> \case
+      '.'  -> [d]
+      '/'  -> case d of
+        N -> [E]
+        W -> [S]
+        S -> [W]
+        E -> [N]
+        _ -> error "encountered a diagonal!"
+      '\\' -> case d of
+        N -> [W]
+        W -> [N]
+        S -> [E]
+        E -> [S]
+        _ -> error "encountered a diagonal!"
+      '|'  -> case d of
+        W -> [N, S]
+        E -> [N, S]
+        o -> [o]
+      '-'  -> case d of
+        N -> [W, E]
+        S -> [W, E]
+        o -> [o]
+      c -> error $ "unexpected character '" ++ [c] ++ "' in input"
+  grid <- ask
+  previous <- get
+  let result = do
+        d' <- directions
+        let p' = p + directionVector d'
+        guard $ inBounds grid p'
+        guard $ not $ S.member (p', d') previous
+        pure (p', d')
+  modify $ S.union (S.fromList result)
+  pure result
+
+energize :: SolveMonad m => OrientedPoint -> m ()
+energize p = void do
+  put $ S.singleton p
+  iterateUntilM null go [p]
+  where
+    go = fmap concat . traverse step
+
+run :: Input -> ReaderT Input (State SolveState) a -> a
+run input action = evalState (runReaderT action input) S.empty
+
+
 part1 :: Input -> Int
-part1 = undefined
+part1 input = run input do
+  energize (Point 0 0, E)
+  gets $ S.size . S.map fst
 
 part2 :: Input -> Int
-part2 = undefined
+part2 input = run input do
+  grid <- ask
+  let w = gWidth  grid
+      h = gHeight grid
+      startingPoints = concat
+        [ [(Point x     0,     S) | x <- [0..w-1]]
+        , [(Point 0     y,     E) | y <- [0..h-1]]
+        , [(Point x     (h-1), N) | x <- [0..w-1]]
+        , [(Point (w-1) y,     W) | y <- [0..h-1]]
+        ]
+  fmap maximum $
+    for startingPoints \p -> do
+      energize p
+      gets $ S.size . S.map fst
 
 
 -- main
 
 example :: String
-example = ""
+example = ".|...\\....\n|.-.\\.....\n.....|-...\n........|.\n..........\n.........\\\n..../.\\\\..\n.-.-/..|..\n.|....-|.\\\n..//.|...."
 
 main :: String -> IO ()
 main rawData = do
   let testInput = parseInput example
       realInput = parseInput rawData
+  putStrLn $ displayWith (const pure) testInput
   putStrLn "# Part 1"
   print $ part1 testInput
   print $ part1 realInput
