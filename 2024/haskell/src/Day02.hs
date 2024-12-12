ef45ad40f08778e6349a0935b2343e7523973572
Antoine Leblanc <crucuny@gmail.com>
2024-12-12 19:54:06 +0000

    2024 haskell day 02

--- 2024/haskell/src/Day02.hs
+++ 2024/haskell/src/Day02.hs
@@ -7,31 +7,50 @@ import AOC
 import "this" Prelude
 
 import Text.Parsec
-import Text.Parsec.Char
 
 
 -- input
 
-type Input = String
+type Input = [Report]
+type Report = [Int]
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = parseLinesWith $ many1 number
 
 
 -- solution
 
+isSafe :: Report -> Bool
+isSafe report = (allIncreasing || allDecreasing) && allCloseEnough
+  where
+    allIncreasing   = and $ zipWith (<)         report (tail report)
+    allDecreasing   = and $ zipWith (>)         report (tail report)
+    allCloseEnough  = and $ zipWith closeEnough report (tail report)
+    closeEnough x y = abs (x-y) <= 3
+
+isPotentiallySafe :: Report -> Bool
+isPotentiallySafe = any isSafe . generateSubLists []
+  where
+    generateSubLists _      []     = []
+    generateSubLists before (x:xs) = (before ++ xs) : generateSubLists (before ++ [x]) xs
+
 part1 :: Input -> Int
-part1 = undefined
+part1 = countIf isSafe
 
 part2 :: Input -> Int
-part2 = undefined
+part2 = countIf isPotentiallySafe
 
 
 -- main
 
 example :: String
-example = ""
+example = "\
+\7 6 4 2 1\n\
+\1 2 7 8 9\n\
+\9 7 6 2 1\n\
+\1 3 2 4 5\n\
+\8 6 4 4 1\n\
+\1 3 6 7 9"
 
 main :: String -> IO ()
 main rawData = do
