f51917060d2066014b02ca09e771198f22207234
Antoine Leblanc <crucuny@gmail.com>
2024-12-12 19:54:24 +0000

    2024 haskell day 04

--- 2024/haskell/src/Day04.hs
+++ 2024/haskell/src/Day04.hs
@@ -4,34 +4,63 @@ module Day04 where
 -- import
 
 import AOC
+import AOC.Grid.Flat
 import "this" Prelude
-
-import Text.Parsec
-import Text.Parsec.Char
 
 
 -- input
 
-type Input = String
+type Input = Grid Char
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = from2DList . lines
 
 
 -- solution
 
 part1 :: Input -> Int
-part1 = undefined
+part1 grid = sum do
+  p <- allPoints grid
+  guard $ (grid ! p) == 'X'
+  d <- [ Point (-1) 0
+       , Point (-1) (-1)
+       , Point    0 (-1)
+       , Point    1 (-1)
+       , Point    1    0
+       , Point    1    1
+       , Point    0    1
+       , Point (-1)    1
+       ]
+  guard $ (grid !? (p + d *. 1)) == Just 'M'
+  guard $ (grid !? (p + d *. 2)) == Just 'A'
+  guard $ (grid !? (p + d *. 3)) == Just 'S'
+  pure 1
 
 part2 :: Input -> Int
-part2 = undefined
+part2 grid = sum do
+  p <- allPoints grid
+  guard $ (grid ! p) == 'A'
+  let d1 = sort $ catMaybes [grid !? (p + Point (-1) (-1)), grid !? (p + Point 1 1)]
+      d2 = sort $ catMaybes [grid !? (p + Point (-1) 1), grid !? (p + Point 1 (-1))]
+  guard $ d1 == "MS"
+  guard $ d2 == "MS"
+  pure 1
 
 
 -- main
 
 example :: String
-example = ""
+example = "\
+\MMMSXXMASM\n\
+\MSAMXMSMSA\n\
+\AMXSXMAAMM\n\
+\MSAMASMSMX\n\
+\XMASAMXAMM\n\
+\XXAMMXXAMA\n\
+\SMSMSASXSS\n\
+\SAXAMASAAA\n\
+\MAMMMXMMMM\n\
+\MXMXAXMASX"
 
 main :: String -> IO ()
 main rawData = do
