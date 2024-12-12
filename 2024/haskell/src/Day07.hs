4f8b35b303d0a1196ea146aa25577e4b195ef933
Antoine Leblanc <crucuny@gmail.com>
2024-12-12 19:54:41 +0000

    2024 haskell day 07

--- 2024/haskell/src/Day07.hs
+++ 2024/haskell/src/Day07.hs
@@ -6,32 +6,59 @@ module Day07 where
 import AOC
 import "this" Prelude
 
+import Data.List.NonEmpty qualified as NE
 import Text.Parsec
-import Text.Parsec.Char
 
 
 -- input
 
-type Input = String
+type Input = [(Int, NonEmpty Int)]
 
 parseInput :: String -> Input
 parseInput = parseLinesWith line
-  where line = undefined
+  where
+    line = do
+      total <- number
+      symbol ":"
+      numbers <- many1 number
+      pure (total, NE.fromList numbers)
 
 
 -- solution
 
 part1 :: Input -> Int
-part1 = undefined
+part1 calibrations = sum do
+  (total, x NE.:| xs) <- calibrations
+  guard $ any (total ==) $ snd $ foldl' step (total, [x]) xs
+  pure total
+  where
+    step (total, accum) x = (total, filter (<= total) $ [(+x), (*x)] <*> accum)
 
 part2 :: Input -> Int
-part2 = undefined
+part2 calibrations = sum do
+  (total, x NE.:| xs) <- calibrations
+  guard $ any (total ==) $ snd $ foldl' step (total, [x]) xs
+  pure total
+  where
+    step (total, accum) x = (total, filter (<= total) $ apply x accum)
+    apply y accum = do
+      x <- accum
+      [x+y, x*y, read (show x ++ show y)]
 
 
 -- main
 
 example :: String
-example = ""
+example = "\
+\190: 10 19\n\
+\3267: 81 40 27\n\
+\83: 17 5\n\
+\156: 15 6\n\
+\7290: 6 8 6 15\n\
+\161011: 16 10 13\n\
+\192: 17 8 14\n\
+\21037: 9 7 18 13\n\
+\292: 11 6 16 20"
 
 main :: String -> IO ()
 main rawData = do
