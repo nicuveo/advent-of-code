19f0a3e7d836c65e5eddbd5abe0561e0a9cd57bb
Antoine Leblanc <crucuny@gmail.com>
2024-12-12 19:53:51 +0000

    2024 haskell day 01

--- 2024/haskell/src/Day01.hs
+++ 2024/haskell/src/Day01.hs
@@ -6,32 +6,41 @@ module Day01 where
 import AOC
 import "this" Prelude
 
-import Text.Parsec
-import Text.Parsec.Char
-
 
 -- input
 
-type Input = String
+type Input = ([Int], [Int])
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = unzip . parseLinesWith line
+  where
+    line = do
+      n1 <- number
+      n2 <- number
+      pure (n1, n2)
 
 
 -- solution
 
 part1 :: Input -> Int
-part1 = undefined
+part1 (l1, l2) = sum $ map abs $ zipWith (-) (sort l1) (sort l2)
 
 part2 :: Input -> Int
-part2 = undefined
+part2 (l1, l2) = sum do
+  x <- l1
+  pure $ x * count x l2
 
 
 -- main
 
 example :: String
-example = ""
+example = "\
+\3   4\n\
+\4   3\n\
+\2   5\n\
+\1   3\n\
+\3   9\n\
+\3   3"
 
 main :: String -> IO ()
 main rawData = do
