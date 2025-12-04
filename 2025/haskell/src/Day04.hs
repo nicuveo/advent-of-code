dfd260c42f37ac583ea6dec3721975baab837363
Antoine Leblanc <crucuny@gmail.com>
2025-12-04 20:25:11 +0000

    2025 Haskell Day 04 (naive approach)

--- 2025/haskell/src/Day04.hs
+++ 2025/haskell/src/Day04.hs
@@ -3,27 +3,49 @@ module Day04 where
 import "this" Prelude
 
 import AOC
-
-import Text.Parsec
-import Text.Parsec.Char
+import AOC.Grid.Flat  as G
 
 
-type Input = String
+type Input = Grid Bool
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = from2DList . map (map ('@' ==)) . lines
 
 
 part1 :: Input -> Int
-part1 = undefined
+part1 g = countTrue do
+  p <- allPoints g
+  guard $ g ! p
+  let n = countTrue $ gridEightNeighbours g p
+  pure $ n < 4
 
 part2 :: Input -> Int
-part2 = undefined
+part2 input = countRolls input - countRolls (go input)
+  where
+    go g =
+      let g' = removeAccessibleRolls g
+      in  if g /= g' then go g' else g'
+
+countRolls :: Input -> Int
+countRolls = countTrue . G.toList
+
+removeAccessibleRolls :: Input -> Input
+removeAccessibleRolls g = flip pmap g \p b ->
+  b && countTrue (gridEightNeighbours g p) >= 4
 
 
 example :: String
-example = ""
+example = "\
+  \..@@.@@@@.\n\
+  \@@@.@.@.@@\n\
+  \@@@@@.@.@@\n\
+  \@.@@@@..@.\n\
+  \@@.@@@@.@@\n\
+  \.@@@@@@@.@\n\
+  \.@.@.@.@@@\n\
+  \@.@@@.@@@@\n\
+  \.@@@@@@@@.\n\
+  \@.@.@@@.@.\n"
 
 main :: String -> IO ()
 main rawData = do
