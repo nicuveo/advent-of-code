735d2389cc1da22e294008b09a9f2ee7ce7ded63
Antoine Leblanc <crucuny@gmail.com>
2025-12-11 19:50:59 +0000

    2025 Haskell Days 09, 10

--- 2025/haskell/src/Day09.hs
+++ 2025/haskell/src/Day09.hs
@@ -1,29 +1,159 @@
 module Day09 where
 
-import "this" Prelude
+import "this" Prelude hiding (intersect)
 
 import AOC
 
-import Text.Parsec
-import Text.Parsec.Char
+import Data.HashSet   qualified as S
+import Data.List      qualified as L
 
 
-type Input = String
+type Input = [Point]
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = parseLinesWith do
+  x <- number
+  symbol ","
+  y <- number
+  pure $ Point x y
 
 
 part1 :: Input -> Int
-part1 = undefined
+part1 allPoints = maximum do
+  (point1:otherPoints) <- L.tails allPoints
+  point2 <- otherPoints
+  let
+    dx = abs (px point2 - px point1) + 1
+    dy = abs (py point2 - py point1) + 1
+    area = dx * dy
+  pure area
 
-part2 :: Input -> Int
-part2 = undefined
+intersect
+  :: HashSet (Point, Point)
+  -> (Point, Point)
+  -> (Point, Point)
+  -> Bool
+intersect safeDirs edge1 edge2
+  | horizontal edge1 && vertical edge2 = check edge1 edge2
+  | horizontal edge2 && vertical edge1 = check edge2 edge1
+  | otherwise = False
+  where
+    horizontal (p1, p2) = py p1 == py p2
+    vertical   (p1, p2) = px p1 == px p2
+    check (h1, h2) (v1, v2)
+      | px v1 < min (px h1) (px h2) = False
+      | px v1 > max (px h1) (px h2) = False
+      | py h1 < min (py v1) (py v2) = False
+      | py h1 > max (py v1) (py v2) = False
+      | strictlyWithin (px v1) (px h1) (px h2) &&
+        strictlyWithin (py h1) (py v1) (py v2) = True
+      | strictlyWithin (px v1) (px h1) (px h2) =
+        if py v1 == py h1
+        then checkInBounds v1 W E
+        else checkInBounds v1 W E
+      | strictlyWithin (py h1) (py v1) (py v2) =
+        if px h1 == px v1
+        then checkInBounds h1 N S
+        else checkInBounds h2 N S
+      | otherwise = False
+    strictlyWithin x v1 v2 =
+      x > min v1 v2 && x < max v1 v2
+    checkInBounds p (directionVector -> e1) (directionVector -> e2) =
+      S.member (p, e1) safeDirs /= S.member (p, e2) safeDirs
 
+data Orientation = CW | CCW
+  deriving (Show, Eq)
+
+orientationSign :: Orientation -> Int
+orientationSign CW  =  1
+orientationSign CCW = -1
+
+computeAngleOrientationSign
+  :: Point
+  -> Point
+  -> Point
+  -> Int
+computeAngleOrientationSign p1 p2 p3 =
+  let dx1 = px p2 - px p1
+      dy1 = py p2 - py p1
+      dx2 = px p3 - px p2
+      dy2 = py p3 - py p2
+  in if | dx1 > 0   ->  signum dy2
+        | dy1 > 0   -> -signum dx2
+        | dx1 < 0   -> -signum dy2
+        | dy1 < 0   ->  signum dx2
+        | otherwise -> error "incorrect input"
+
+computeShapeOrientation :: [Point] -> Orientation
+computeShapeOrientation allPoints = validate $ sum do
+  (p1:p2:p3:_) <- L.tails $ allPoints ++ take 2 allPoints
+  pure $ computeAngleOrientationSign p1 p2 p3
+  where
+    validate :: Int -> Orientation
+    validate = \case
+      4  -> CW
+      -4 -> CCW
+      x  -> error $ "got incorrect value: " ++ show x
+
+computePointInfo
+  :: [Point]
+  -> Orientation
+  -> HashSet (Point, Point)
+computePointInfo allPoints shapeOrientation = S.fromList do
+  (p1:p2:p3:_) <- L.tails $ allPoints ++ take 2 allPoints
+  let unitVector = signum $ p2 - p1
+      os = computeAngleOrientationSign p1 p2 p3
+  if | os /= orientationSign shapeOrientation ->
+       (p2,) . directionVector <$> [N, S, E, W]
+     | shapeOrientation == CW ->
+       (p2,) <$> [turn90R unitVector, -unitVector]
+     | otherwise ->
+       (p2,) <$> [turn90L unitVector, -unitVector]
+
+part2 :: Input -> (Int, (Point, Point))
+part2 allPoints = maximumOn fst do
+  (p1:otherPoints) <- L.tails allPoints
+  p2 <- otherPoints
+  let
+    p3 = Point (px p2) (py p1)
+    p4 = Point (px p1) (py p2)
+    edges =
+      [ (p1, p3)
+      , (p3, p2)
+      , (p2, p4)
+      , (p4, p1)
+      ]
+  guard $ (p1, signum (p3 - p1)) `S.member` safeDirections
+  guard $ (p1, signum (p4 - p1)) `S.member` safeDirections
+  guard $ (p2, signum (p3 - p2)) `S.member` safeDirections
+  guard $ (p2, signum (p4 - p2)) `S.member` safeDirections
+  guard $ and do
+    rectangleEdge <- edges
+    outlineEdge   <- outline
+    pure $ not $ intersect safeDirections rectangleEdge outlineEdge
+  let
+    dx = abs (px p2 - px p1) + 1
+    dy = abs (py p2 - py p1) + 1
+    area = dx * dy
+  pure (area, (p1, p2))
+  where
+    shapeOrientation = computeShapeOrientation allPoints
+    safeDirections = computePointInfo allPoints shapeOrientation
+    outline = do
+      (p1:p2:_) <- L.tails $ allPoints ++ take 1 allPoints
+      pure (p1, p2)
 
 example :: String
-example = ""
+example = "\
+  \7,1\n\
+  \11,1\n\
+  \11,7\n\
+  \9,7\n\
+  \9,5\n\
+  \2,5\n\
+  \2,3\n\
+  \7,3"
+
 
 main :: String -> IO ()
 main rawData = do
--- 2025/haskell/src/Day10.hs
+++ 2025/haskell/src/Day10.hs
@@ -4,26 +4,104 @@ import "this" Prelude
 
 import AOC
 
+import Control.Parallel.Strategies
+import Data.Bits
+import Data.List.Extra             qualified as L
+import Data.Maybe
+import Data.SBV                    as SBV
+import Data.SBV.List               qualified as SBV
 import Text.Parsec
-import Text.Parsec.Char
 
 
-type Input = String
+type Input = [Machine]
+
+data Machine = Machine
+  { desiredLights  :: Int
+  , desiredJoltage :: [Int]
+  , buttons        :: [[Int]]
+  }
+  deriving Show
 
 parseInput :: String -> Input
-parseInput = parseLinesWith line
-  where line = undefined
+parseInput = parseLinesWith do
+  l <- lights
+  b <- many button
+  j <- joltage
+  pure $ Machine l j b
+  where
+    lights = do
+      symbol "["
+      bits <- many1 light
+      symbol "]"
+      pure $ sum do
+        (index, b) <- zip [0..] bits
+        pure $ b * 2 ^ (index :: Int)
+    light = choice
+      [ 1 <$ char '#'
+      , 0 <$ char '.'
+      ]
+    button = do
+      symbol "("
+      indices <- number `sepBy` symbol ","
+      symbol ")"
+      pure indices
+    joltage = do
+      symbol "{"
+      indices <- number `sepBy` symbol ","
+      symbol "}"
+      pure indices
 
+
+findShortestLightsSequence :: Machine -> Int
+findShortestLightsSequence Machine {..} = go 0 [0]
+  where
+    buttonValues :: [Int]
+    buttonValues = map (sum . map (2 ^)) buttons
+
+    go :: Int -> [Int] -> Int
+    go !steps values
+      | desiredLights `elem` values = steps
+      | otherwise =
+          go (steps + 1) $ L.nubOrd do
+            v <- values
+            b <- buttonValues
+            pure $ v `xor` b
+
+findShortestJoltageSequence :: Machine -> IO Word16
+findShortestJoltageSequence Machine {..} = do
+  model <- optLexicographic do
+    (buttonVariables, columns) <-
+      unzip <$> for buttons \button -> do
+        buttonVar <- sWord16_
+        let column = do
+              index <- [0..length desiredJoltage-1]
+              pure $ if index `elem` button
+                     then Just buttonVar
+                     else Nothing
+        constrain $ buttonVar .<= fromIntegral maxPresses
+        pure (buttonVar, column)
+    let rows = map (SBV.implode . catMaybes) $ L.transpose columns
+    for_ (zip rows desiredJoltage) \(row, value) -> do
+      SBV.constrain $ varSum row .== fromIntegral value
+    minimize resultName $ varSum $ SBV.implode buttonVariables
+  pure $ fromJust $ getModelValue resultName model
+  where
+    maxPresses = maximum desiredJoltage
+    resultName = "fewest presses"
+    varSum = SBV.foldl (+) 0
 
 part1 :: Input -> Int
-part1 = undefined
+part1 = sum . parMap rpar findShortestLightsSequence
 
-part2 :: Input -> Int
-part2 = undefined
+part2 :: Input -> IO Word16
+part2 = fmap (sum . withStrategy (evalList rpar)) . traverse findShortestJoltageSequence
 
 
 example :: String
-example = ""
+example = "\
+  \[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n\
+  \[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n\
+  \[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
 
 main :: String -> IO ()
 main rawData = do
@@ -33,5 +111,5 @@ main rawData = do
   print $ part1 testInput
   print $ part1 realInput
   putStrLn "# Part 2"
-  print $ part2 testInput
-  print $ part2 realInput
+  print =<< part2 testInput
+  print =<< part2 realInput
